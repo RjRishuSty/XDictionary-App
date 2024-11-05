32618ac59cd8cf61a665a832e928abdc1a6a6aa6
Your Name <rishurajk59@gmail.com>
2024-11-05 21:54:37 +0530

    add xDictionary project

---
+++ xdictionary/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import XDictionary from "./Component/XDictionary/XDictionary";
+
+const App = () => {
+  return (
+    <div className="App">
+      <XDictionary />
+    </div>
+  );
+};
+
+export default App;
---
+++ xdictionary/src/Component/XDictionary/XDictionary.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from "react";
+import Styles from "./XDictionary.module.css";
+import { data } from "../../WordData.js";
+
+const XDictionary = () => {
+  const inputRef = useRef();
+  const [search, setSearch] = useState("");
+  const [filterData, setFilterData] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const filter = data.find((item) =>
+      item.word.toLowerCase().includes(search)
+    );
+    if (filter) {
+      setFilterData(filter.meaning);
+    } else {
+      setFilterData("Word not found in the dictionary.");
+    }
+  };
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+  //   console.log("search", search);
+  return (
+    <div className={Styles.card}>
+      <h1 className={Styles.heading}>Dictionary App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className={Styles.input}
+          type="text"
+          placeholder="Search for a word"
+          ref={inputRef}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+        <button className={Styles.button}>Search</button>
+      </form>
+      {filterData && (
+        <div className={Styles.result}>
+          <p>
+            <strong>Definition: </strong>
+            {filterData}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default XDictionary;
